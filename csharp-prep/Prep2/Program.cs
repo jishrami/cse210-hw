a6c6a79b2154f03e2d4d118774aa9747e3516393
jishrami <jramirez@byui.edu>
2024-01-11 13:09:21 -0700

    everything except stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string inGradePerc = Console.ReadLine();
+        int valueGradePerc = int.Parse(inGradePerc);
+        string letterGrade;
+        string gradeSuffix;
+        if (valueGradePerc >= 0 && valueGradePerc <= 100)
+        {
+            if (valueGradePerc >= 70)
+            {
+                Console.WriteLine("You passed!");
+                if (valueGradePerc < 80)
+                {
+                    letterGrade = "C";
+                }
+                else if (valueGradePerc < 90)
+                {
+                    letterGrade = "B";
+                }
+                else
+                {
+                    letterGrade = "A";
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not pass. Try again!");
+                if (valueGradePerc >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+            }
+            
+            Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
     }
 }

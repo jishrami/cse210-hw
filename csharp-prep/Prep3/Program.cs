26a8adb6e7a801de78c25b4fd173cbe37a0969e6
jishrami <jramirez@byui.edu>
2024-01-16 13:02:37 -0700

    prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        string response = "yes";
+        do
+        {
+            Random number = new Random();
+            int magicNumber = number.Next(1,100);
+            Console.WriteLine(magicNumber);
+            int numberGuess;
+            int amountGuess = 0;
+                do
+                {
+                    Console.Write("What is your guess? ");
+                    numberGuess = int.Parse(Console.ReadLine());
+                    
+                    if (numberGuess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else if (numberGuess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (numberGuess == magicNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+                    amountGuess += 1;
+                } while (numberGuess != magicNumber);
+
+            if (amountGuess > 1)
+            {
+                Console.WriteLine($"You took {amountGuess} tries to guess");
+            }
+            else if (amountGuess == 1)
+            {
+                Console.WriteLine($"You took {amountGuess} try to guess");
+            }
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response.ToLower() == "yes");
     }
 }

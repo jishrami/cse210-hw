142544f7dbb074bb16ab9aa0dd335588153b75a7
jishrami <jramirez@byui.edu>
2024-01-17 10:23:34 -0700

    core requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int listInput;
+        List<int> userInput;
+        userInput = new List<int>();
+        int sumInput = 0;
+        int largeNum = 0;
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter a number: ");
+            listInput = int.Parse(Console.ReadLine());
+            if (listInput != 0)
+            {
+                userInput.Add(listInput);
+            }
+            sumInput += listInput;
+            if (listInput > largeNum)
+            {
+                largeNum = listInput;
+            }
+
+        }while (listInput != 0);
+
+        float avgNum = (float)sumInput / userInput.Count;
+
+        Console.WriteLine($"The sum is: {sumInput}");
+        Console.WriteLine($"The average is: {avgNum}");
+        Console.WriteLine($"The largest number is: {largeNum}");
     }
 }

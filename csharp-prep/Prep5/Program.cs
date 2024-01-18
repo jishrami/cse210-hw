03d76477f8867ec04249ea5d7c51f2d254a021ff
jishrami <jramirez@byui.edu>
2024-01-18 13:07:03 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine(); 
+        return userName;
+    }
+
+    static double SquaredNumber(int baseNumber, int sqr=2)
+    {
+        return Math.Pow(baseNumber, sqr);
+    }
+
+    static int EnteredNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int enteredNumber = int.Parse(Console.ReadLine());
+        return enteredNumber;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string usrname = UserName();
+        int entrNumber = EnteredNumber();
+        double sqrNumber = SquaredNumber(entrNumber);
+        
+        Console.WriteLine($"{usrname}, the square of your number is {sqrNumber}");
     }
 }

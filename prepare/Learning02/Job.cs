bc0f1cc0da734b72c101cbee614215bdb6cad30a
jishrami <jramirez@byui.edu>
2024-01-22 21:53:23 -0700

    started work, created job and called it in program

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+public class Job {
+    public string _jobTitle;
+    public string _companyName;
+    public string _dateHired;
+    public string _dateLeft;
+
+    // public string DisplayJob()
+    // {
+
+    //     return 
+    // }
+}
+
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._dateHired = "2024";
+        job1._dateLeft = "Present";
+
+        Console.WriteLine($"{job1._jobTitle} ({job1._companyName}) {job1._dateHired}-{job1._dateLeft}");
     }
 }

74b835764dd0bcabb1cf376810e1871610e1fd86
jishrami <jramirez@byui.edu>
2024-02-01 13:04:20 -0700

    added prompts

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,7 +11,12 @@ class Program
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
+            "If I had one thing I could do over today, what would it be?",
+            "What is a new skill or knowledge I acquired today?",
+            "Describe a moment that brought you joy or made you smile today.",
+            "How did I handle a challenging situation today, and what did I learn from it?",
+            "What is something you are grateful for today?",
+            "Reflect on a meaningful conversation you had today and its impact."
         };
         List<Entries> journalEntries = new List<Entries>();
         int userInput = 0;

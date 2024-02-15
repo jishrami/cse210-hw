4876668a44ab3ad36da521d2425f4392b55f855c
jishrami <jramirez@byui.edu>
2024-02-15 15:07:11 -0700

    iterate and break correctly

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,9 +7,13 @@ class Program
         Reference reference1 = new Reference("John", 3, 16);
         Reference reference2 = new Reference("John", 3, 16, 18);
         Reference reference3 = new Reference("2 Nephi", 2, 13);
+        Reference test = new Reference("test", 1, 1, 1);
         Scripture scripture1 = new Scripture(reference1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
         Scripture scripture2 = new Scripture(reference2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life. For God sent not his Son into the world to condemn the world; but that the world through him might be saved. He that believeth on him is not condemned: but he that believeth not is condemned already, because he hath not believed in the name of the only begotten Son of God.");
         Scripture scripture3 = new Scripture(reference3, "And if ye shall say there is no law, ye shall also say there is no sin. If ye shall say there is no sin, ye shall also say there is no righteousness. And if there be no righteousness there be no happiness. And if there be no righteousness nor happiness there be no punishment nor misery. And if these things are not there is no God. And if there is no God we are not, neither the earth; for there could have been no creation of things, neither to act nor to be acted upon; wherefore, all things must have vanished away.");
+        Scripture testScripture = new Scripture(test, "test test test test");
+
+        List<Word> hiddenWords = new List<Word>();
 
         //names a scripture to reference, change the last digit to change the reference
         Scripture calledScripture = scripture1;
@@ -22,29 +26,36 @@ class Program
 
         //creates a new list in Word by separating the words into individual strings
         List<Word> words = calledScripture.GetWords();
-
+        int iterate = words.Count() / 3;
+        int wordCount = words.Count();
+        int hiddenWordCount = hiddenWords.Count();
         //checks to see if any words are not hidden, if they are, it will randomly hide a word, if not, it will quit
-        while (AnyNotHidden(words))
+        for (int a = 0; a < iterate+1; a++)
         {
-            foreach (Word word in words)
+            for (int i = 0; i < 3; i++)
             {
-                //if the word is not hidden, it will randomly choose to hide it or not
-                if (!word.IsHidden())
+                Random random = new Random();
+                int randomIndex = random.Next(0, wordCount);
+                if (wordCount == hiddenWordCount)
                 {
-                    Random random = new Random();
-                    if (random.Next(2) == 1)
-                    {
-                        word.Hide();
-                    }
-                }
-            }
+                    break;
+                }   
 
+                while (hiddenWords.Contains(words[randomIndex]))
+                {
+                    randomIndex = random.Next(0, wordCount);
+                }
+
+                words[randomIndex].Hide();
+                hiddenWords.Add(words[randomIndex]);
+                hiddenWordCount++;
+            }
             Console.Clear();
             Console.WriteLine(calledScripture.GetRenderedText());
             Console.WriteLine("Type 'quit' to exit the program.");
             string input = Console.ReadLine();
 
-            if (input.ToLower() == "quit")
+            if (input.ToLower() == "quit" || a == iterate)
             {
                 Console.Clear();
                 break;

29eacd05d273c6a926910ab9f60c2e5b2eac3ec7
jishrami <jramirez@byui.edu>
2024-03-07 10:42:57 -0700

    added list

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,16 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Shape> shapes = new List<Shape>();
+
         Shape square = new Square("yellow", 3.5);
-        Console.WriteLine(square.GetColor());
-        Console.WriteLine(square.GetArea());
+        shapes.Add(square);
 
         Shape rectangle = new Rectangle("blue", 3.5, 4.5);
-        Console.WriteLine(rectangle.GetColor());
-        Console.WriteLine(rectangle.GetArea());
+        shapes.Add(rectangle);
 
         Shape circle = new Circle("red", 3.5);
-        Console.WriteLine(circle.GetColor());
-        Console.WriteLine(circle.GetArea());
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
